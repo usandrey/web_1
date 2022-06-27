e9a39563228779a3fe0e0775fe1758af45ef6294
ys_andrey <usandrey1@gmail.com>
2022-06-27 15:41:51 +0300

    added carusel and price

--- crm/models.py
+++ crm/models.py
@@ -12,3 +12,13 @@ class Order(models.Model):
     class Meta:
         verbose_name = 'Заказ'
         verbose_name_plural = 'Заказы'
+
+class StatusCrm(models.Model):
+    Status_name = models.CharField(max_length=200, verbose_name='Название статуса')
+
+    def __dir__(self):
+        return self.Status_name
+
+    class Meta:
+        verbose_name = 'Статус'
+        verbose_name_plural = 'Статусы'
